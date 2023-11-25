4f75746948b6f469ceff5befcb97695ee772ec8a
Jamie <Jay.mudle@gmail.com>
2023-11-25 10:50:26 +0000

    JM: updated README and progress bar

--- Music_Importer.py
+++ Music_Importer.py
@@ -1,8 +1,10 @@
+#import modules for use.
 import os
 import shutil
 from tinytag import TinyTag
 from tqdm import tqdm
 
+#import selected metadata from files iterating through directories.
 def import_metadata(directory="."):
     global audio_files
     audio_files = []
@@ -18,13 +20,14 @@ def import_metadata(directory="."):
                     'full_path': os.path.join(root, file)
                 }
                 audio_files.append(audio_info)
-
+    # error handling for empty directory
     if not audio_files:
         print("No audio files found in the specified directory or its subdirectories.")
         exit()
 
 import_metadata()
 
+# Make new directory based on artist and album if it doesn't exist already and move files there.
 def make_and_move():
     for audio_info in tqdm(audio_files, desc='Processing', unit='file'):
         full_path = os.path.join(audio_info['Artist_Var'], audio_info['Album_Var'], f"{audio_info['Title_Track']}.mp3")
@@ -33,7 +36,7 @@ def make_and_move():
             os.makedirs(os.path.join(audio_info['Artist_Var'], audio_info['Album_Var']))
         
         shutil.move(audio_info['full_path'], full_path)
-        tqdm.write(f"{audio_info['Title_Track']} moved successfully to {full_path}")
+        tqdm.write(f"{audio_info['Title_Track']} moved successfully to {full_path}") # Print out progress as files are moved
 
 make_and_move()
 
